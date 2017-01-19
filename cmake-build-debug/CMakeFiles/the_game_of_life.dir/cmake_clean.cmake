fb2680af3c388efa2ebb7046faa5adacf543f006
panagiotappl <panagiotappl@gmail.com>
2017-01-19 12:29:54 +0200

    First commit

---
+++ cmake-build-debug/CMakeFiles/the_game_of_life.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/the_game_of_life.dir/main.c.o"
+  "the_game_of_life.pdb"
+  "the_game_of_life"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/the_game_of_life.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
