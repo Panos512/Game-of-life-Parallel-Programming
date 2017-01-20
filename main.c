be769868cc9dba8e16485d8c0b2eabda797ceda4
panagiotappl <panagiotappl@gmail.com>
2017-01-20 23:51:48 +0200

    get_ranks impl

--- main.c
+++ main.c
@@ -2,6 +2,40 @@
 #include <stdlib.h>
 #include <mpi.h>
 #include <math.h>
+typedef enum {DIR_ULEFT = 0, DIR_UP, DIR_URIGHT, DIR_LEFT, DIR_CENTER,
+    DIR_RIGHT, DIR_DLEFT, DIR_DOWN, DIR_DRIGHT} dir;
+// store the ranks of neighbouring processes in ranks[]
+void get_ranks(int cur_rank, MPI_Comm cartesian, int ranks[]) {
+    int coords[2];
+
+    //Vres tis suntetagmenes mou
+    MPI_Cart_coords(cartesian, cur_rank, 2, coords);
+    ranks[DIR_CENTER] = cur_rank;
+
+    //Aptis suntetagmenes mou upologise tis suntetagmenes tou kathe geitona
+    //kai vres to rank tou
+    //O pinakas rank perilamvanei:
+    //rank[DIR_ULEFT, DIR_UP, DIR_URIGHT, DIR_LEFT, DIR_CENTER,
+    //DIR_RIGHT, DIR_DLEFT, DIR_DOWN, DIR_DRIGHT]
+    coords[1]--;
+    coords[0]--;
+    MPI_Cart_rank(cartesian, coords, &ranks[DIR_ULEFT]);
+    coords[1]++;
+    MPI_Cart_rank(cartesian, coords, &ranks[DIR_UP]);
+    coords[1]++;
+    MPI_Cart_rank(cartesian, coords, &ranks[DIR_URIGHT]);
+    coords[0]++;
+    MPI_Cart_rank(cartesian, coords, &ranks[DIR_RIGHT]);
+    coords[1] -= 2;
+    MPI_Cart_rank(cartesian, coords, &ranks[DIR_LEFT]);
+    coords[0]++;
+    MPI_Cart_rank(cartesian, coords, &ranks[DIR_DLEFT]);
+    coords[1]++;
+    MPI_Cart_rank(cartesian, coords, &ranks[DIR_DOWN]);
+    coords[1]++;
+    MPI_Cart_rank(cartesian, coords, &ranks[DIR_DRIGHT]);
+}
+
 
 char* create_buffer(char* buffer, int width, int height, char** argv, int argc){
     if(argc == 2){
@@ -35,6 +69,8 @@ char* create_buffer(char* buffer, int width, int height, char** argv, int argc){
 
 }
 int main(int argc, char **argv) {
+    MPI_Comm cartesian;
+
     int size, rank;
     int width, height;
 
@@ -73,6 +109,17 @@ int main(int argc, char **argv) {
     int blockwidth = width / blockside;
     int blockheight = height / blockside;
 
+    int sides[2] = {blockside,blockside};
+    int periodic[2] = {1, 1};
+
+    MPI_Cart_create(MPI_COMM_WORLD, 2, sides, periodic, 1, &cartesian);
+    if (cartesian == MPI_COMM_NULL) {
+        printf("Could not create cartesian communicator\n");
+        MPI_Finalize();
+        return 1;
+    }
+
+
     //Allocate local buffer of each process
     char local_buffer[blockheight*blockwidth];
     for (int i = 0; i <  blockheight*blockwidth; i ++)
@@ -104,8 +151,9 @@ int main(int argc, char **argv) {
     }
 
     //Scatter blocks to processes
-    MPI_Scatterv(buffer, counts, disps, blocktype, local_buffer, blockheight*blockwidth, MPI_CHAR, 0, MPI_COMM_WORLD);
-
+    MPI_Scatterv(buffer, counts, disps, blocktype, local_buffer, blockheight*blockwidth, MPI_CHAR, 0, cartesian);
+    int neighbour_ranks[9];
+    get_ranks(rank,  cartesian, neighbour_ranks);
 
     for (int proc=0; proc<size; proc++) {
         if (proc == rank) {
@@ -119,6 +167,10 @@ int main(int argc, char **argv) {
                     printf("\n");
                 }
             }
+            printf("My Neighbors: \n");
+            for(int i = 0; i < 9; i++)
+                printf("%d ", neighbour_ranks[i]);
+            printf("\n");
             printf("Local Matrix: \n");
             for (int ii=0; ii<blockheight; ii++) {
                 for (int jj=0; jj<blockwidth; jj++) {
